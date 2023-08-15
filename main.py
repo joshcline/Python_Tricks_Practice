763edb0e01a378ded525a824087629f25c9eef3a
Josh Cline <joshccline@outlook.com>
2023-08-15 19:21:12 -0400

    Completed problems 4 & 5.

--- main.py
+++ main.py
@@ -14,3 +14,12 @@ print(cubed([3, 6, 9, 2]))
 ## List after Lambda Function and list comprehension: [False, True, False, True]
 even_odd = lambda x: True if x%2 == 0 else False
 print([even_odd(x) for x in [3, 6, 9, 2]])
+
+# Problem 4: Use a list comprehension to create a list of the numbers from 1 to 100 (including 100).
+nums = [i for i in range(1, 101)]
+print(nums)
+
+# Problem 5: Use a dictionary comprehension to count the length of each word in a sentence.
+## Input: "The quick brown fox jumped over the fence." output: {'The': 3, 'quick': 5, 'brown': 5, 'fox': 3, 'jumped': 6, 'over': 4, 'the': 3, 'fence.': 6}
+sent = "The quick brown fox jumped over the fence."
+print ({word:len(word) for word in sent.split()})
